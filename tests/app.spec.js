d45c59206166546c0fe6f3e954fc3501d831269c
supaket <supaket@enersys.co.th>
2023-05-26 10:16:31 +0700

    fix bug

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -4,7 +4,7 @@ const app = require('../app');
 describe('GET /healthcheck', () => {
   it('responds with status: UP', async () => {
     const response = await request(app).get('/healthcheck');
-    expect(response.statusCode).toBe(400);
+    expect(response.statusCode).toBe(200);
     expect(response.body).toEqual({ status: 'UP' });
   });
 });
